3569399996b64f52db4395fcb2d5a3aca3f291a4
murodov109 <akabekov77@gmail.com>
2025-10-29 19:38:05 +0500

    Remove keep_alive function and polling call
    Removed keep_alive function and its invocation.

--- bot.py
+++ bot.py
@@ -433,9 +433,4 @@ def home():
     return "Bot ishlayapti"
 def run():
     app.run(host="0.0.0.0", port=8080)
-def keep_alive():
-    t = threading.Thread(target=run)
-    t.start()
 
-keep_alive()
-bot.polling(none_stop=True, interval=0, timeout=20)
